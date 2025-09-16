ee087d02c3bca87370e35d22321f8117faca0245
Rory Jarrard <rjarrard@gmail.com>
2025-09-16 15:22:17 -0600

    create expenses components

---
+++ src/components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+function ExpensesSumnmary() {
+    return (
+        <View>
+            <Text>Expenses Summary</Text>
+            <Text>Total: $177.95</Text>
+        </View>
+    );
+}
+
+export default ExpensesSumnmary;
